65516f68f2810fab607263c709e057827f739f10
Wiam <wiamnasr@gmail.com>
2021-12-17 12:12:14 +0000

    post method and handling form data

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,43 +1,25 @@
 const express = require("express");
 const app = express();
 
-// dependency, third party middleware! <3
-const morgan = require("morgan");
+let { people } = require("./data");
 
-// Middlewares
-const logger = require("./logger");
-const authorize = require("./authorize");
+// static assets (we have a working app in this path)
+app.use(express.static("./methods-public"));
 
-// referencing my middleware function =>  app.use will invoke my logger for anything after it (order matters, remember!)
-// can provide a path that will apply to anything that comes after the path that we provide (here /api for example can be passed as a first argument to the app.use function)
-// If we omit the path, it will be applied to all our requests
-// app.use can be added to specific routes directly
+// parse form data middleware
+app.use(express.urlencoded({ extended: false }));
 
-// req => middleware => response
-// app.use([logger, authorize]);
-// api/home/about/products
-
-// static middleware
-// app.use(express.static("./public"));
-
-// using morgan for all routes, 'tiny' provides most essential data only (method, route, status, how long it took for the server to respond in ms)
-app.use(morgan("tiny"));
-
-app.get("/", (req, res) => {
-  res.send("Home");
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get("/about", (req, res) => {
-  res.send("About");
-});
-
-app.get("/api/products", (req, res) => {
-  res.send("Products");
-});
-
-app.get("/api/items", [logger, authorize], (req, res) => {
-  console.log(req.user);
-  res.send("Items");
+// Inserting data with a post method
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please provide Credentials...");
 });
 
 app.listen(5000, () => {
