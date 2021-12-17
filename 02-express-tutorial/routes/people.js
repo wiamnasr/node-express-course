189cb58fa14dd5528fbb7e8ee3d43c648deb717b
Wiam <wiamnasr@gmail.com>
2021-12-17 15:03:47 +0000

    chaining route methods for matching urls

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -11,16 +11,21 @@ const {
   deletePerson,
 } = require("../controllers/people");
 
-router.get("/", getPeople);
+// Leaner way of setting up routes, same functionality but here we are "chaining" our methods for matching urls
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 
-router.post("/", createPerson);
+// router.get("/", getPeople);
 
-router.post("/postman", createPersonPostman);
+// router.post("/", createPerson);
 
-// PUT http method to update data (editing the data)
-router.put("/:id", updatePerson);
+// router.post("/postman", createPersonPostman);
 
-// Delete http method (similar to put but will not de-structure the params objects here, instead directly accessing it)
-router.delete("/:id", deletePerson);
+// // PUT http method to update data (editing the data)
+// router.put("/:id", updatePerson);
+
+// // Delete http method (similar to put but will not de-structure the params objects here, instead directly accessing it)
+// router.delete("/:id", deletePerson);
 
 module.exports = router;
