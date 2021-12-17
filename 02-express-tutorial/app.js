9ae28d5aa801a825758f01106d6fbde609dac94f
Wiam <wiamnasr@gmail.com>
2021-12-17 14:10:51 +0000

    PUT http method is cool

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -9,8 +9,31 @@ app.use(express.static("./methods-public"));
 // parse form data middleware
 app.use(express.urlencoded({ extended: false }));
 
+// parse json
+app.use(express.json());
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
+  }
+  res.status(201).json({ success: true, person: name });
+});
+
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
+  }
+  res.status(201).send({ success: true, data: [...people, name] });
 });
 
 // Inserting data with a post method
@@ -20,6 +43,28 @@ app.post("/login", (req, res) => {
     return res.status(200).send(`Welcome ${name}`);
   }
   res.status(401).send("Please provide Credentials...");
+});
+
+// PUT http method to update data (editing the data)
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
